35f3bd8c983276101794d10540b93054416dc3a1
700133262 <700133262@APPG20046>
2024-11-20 11:58:46 -0600

    made board look better

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -11,11 +11,11 @@
 
 void TicTacToe::Display() const
 {
-	std::cout << " " << m_board[0]<< " |" << m_board[1] <<"|"<< m_board[2]<< " \n" ;
-	std::cout << "- - - \n"; 
-	std::cout << " " << m_board[3] << " |" << m_board[4] << "|" << m_board[5] << " \n";
-	std::cout << "- - -\n";
-	std::cout << " " << m_board[6] << " |" << m_board[7] << "|" << m_board[8] << "\n";;
+	std::cout << " " << m_board[0]<< " | " << m_board[1] <<" | "<< m_board[2]<< " \n" ;
+	std::cout << "----------- \n"; 
+	std::cout << " " << m_board[3] << " | " << m_board[4] << " | " << m_board[5] << " \n";
+	std::cout << "----------- \n"; 
+	std::cout << " " << m_board[6] << " | " << m_board[7] << " | " << m_board[8] << "\n";;
 
 }
 void  TicTacToe::TakeTurn()
